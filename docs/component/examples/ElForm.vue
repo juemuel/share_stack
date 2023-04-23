a57cb6f15eeac7da0d6018ba90ef7af84197eefd
juemuel <799525644@qq.com>
2023-04-23 22:44:34 +0800

    update

--- docs/component/examples/ElForm.vue
+++ docs/component/examples/ElForm.vue
@@ -10,22 +10,26 @@
 				<el-option label="2班" value="1002"></el-option>
 			</el-select>
 		</el-form-item>
-		<el-form-item label="单选框Radio">
-			<el-radio v-model="radio" label="11" @change="handleRadioChange">选项1</el-radio>
-			<el-radio v-model="radio" label="12" @change="handleRadioChange">选项2</el-radio>
+		<el-form-item label="单选Radio">
+			<el-radio v-model="radio1" label="11" @change="handleRadioChange">选项1</el-radio>
+			<el-radio v-model="radio1" label="12" @change="handleRadioChange">选项2</el-radio>
 		</el-form-item>
-		<el-radio-group v-model="radio" @change="handleRadioChange">
-			<el-radio label="21">选项1</el-radio>
-			<el-radio label="22">选项2</el-radio>
-			<el-radio label="23">选项3</el-radio>
-		</el-radio-group>
-		<el-checkbox-group v-model="checkList">
-		    <el-checkbox label="蓝桥云课 A"></el-checkbox>
-		    <el-checkbox label="蓝桥云课 B"></el-checkbox>
-		    <el-checkbox label="蓝桥云课 C"></el-checkbox>
-		    <el-checkbox label="蓝桥云课 E" disabled></el-checkbox>
-		    <el-checkbox label="蓝桥云课 E" disabled></el-checkbox>
+		<el-form-item label="单选RadioGroup">
+			<el-radio-group v-model="radio2" @change="handleRadioChange">
+				<el-radio label="21">选项1</el-radio>
+				<el-radio label="22">选项2</el-radio>
+				<el-radio label="23">选项3</el-radio>
+			</el-radio-group>
+		</el-form-item>
+		<el-form-item label="多选框(最少1最多2)">
+		<el-checkbox-group v-model="checkList" :min="1" :max="2">
+		    <el-checkbox label="31"></el-checkbox>
+		    <el-checkbox label="32"></el-checkbox>
+		    <el-checkbox label="33"></el-checkbox>
+		    <el-checkbox label="34" disabled></el-checkbox>
+		    <el-checkbox label="35" disabled></el-checkbox>
 		  </el-checkbox-group>
+		</el-form-item>
 		<el-form-item>
 			<el-button type="primary">提交</el-button>
 			<el-button>取消</el-button>
@@ -41,7 +45,9 @@
 					name: '',
 					classId: '',
 				},
-        radio: "1",
+        radio1: "11",
+        radio2: "21",
+				checkList: ["31", "32"],
 			}
 		},
     methods: {
